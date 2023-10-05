49580a1906bc95a97ca27ea69fe24e834a2e98d9
Rebeca Eduarda de Oliveira Santos <20211114010006@ifrn.local>
2023-10-05 17:27:13 -0300

    #2 funcionabilidade navbar inicio da telinha de disciplinas

--- app/src/main/java/br/edu/ifrn/sc/info/istudy/telas/TelaPrincipal.java
+++ app/src/main/java/br/edu/ifrn/sc/info/istudy/telas/TelaPrincipal.java
@@ -1,32 +1,62 @@
 package br.edu.ifrn.sc.info.istudy.telas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import br.edu.ifrn.sc.info.istudy.R;
+import br.edu.ifrn.sc.info.istudy.databinding.ActivityTelaInicialBinding;
+import br.edu.ifrn.sc.info.istudy.databinding.ActivityTelaPrincipalBinding;
+
 
 public class TelaPrincipal extends AppCompatActivity {
 
-    TextView iStudyLogo;
+    private ImageView retorno;
+    private TextView iStudyLogo;
+    private ActivityTelaPrincipalBinding binding;
+
+    private NavHostFragment navHostFragment;
+    private NavController navController;
+
+    private Bundle bundle = new Bundle();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_principal);
         getSupportActionBar().hide();
+        binding = ActivityTelaPrincipalBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initNavigation();
 
         iStudyLogo = findViewById(R.id.iStudyLogo);
+        retorno = findViewById(R.id.voltar);
 
         String i = getColoredSpanned("i", "#7EBA2F");
         String study = getColoredSpanned("Study","#A547F9");
 
         iStudyLogo.setText(Html.fromHtml(i+study));
+
+        retorno.setVisibility(View.INVISIBLE);
     }
 
     private String getColoredSpanned(String text, String color) {
         String input = "<font color=" + color + ">" + text + "</font>";
         return input;
     }
+
+    private void initNavigation(){
+        navHostFragment
+                = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.frame_layout);
+        navController = navHostFragment.getNavController();
+        navController.setGraph(R.navigation.nav_principal, bundle);
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController);
+    }
+
 }
