7b5532e217c3bcf8d54d1351d786f21d51f82aa1
[edvaldinhs] <[edvaldohenriqueceu@gmail.com]>
2023-10-21 18:11:31 -0300

    #3 Adição de Animação e Imã para as disciplinas.

--- app/src/main/java/br/edu/ifrn/sc/info/istudy/telas/fragment/Home.java
+++ app/src/main/java/br/edu/ifrn/sc/info/istudy/telas/fragment/Home.java
@@ -1,21 +1,23 @@
 package br.edu.ifrn.sc.info.istudy.telas.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
 
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,7 +26,6 @@ import br.edu.ifrn.sc.info.istudy.adapters.AdapterDisciplinas;
 import br.edu.ifrn.sc.info.istudy.adapters.holders.click.OnDisciplinaClickListener;
 import br.edu.ifrn.sc.info.istudy.dominio.Disciplina;
 import br.edu.ifrn.sc.info.istudy.retrofit.RetrofitConfig;
-import br.edu.ifrn.sc.info.istudy.telas.TelaInicial;
 import br.edu.ifrn.sc.info.istudy.ws.DisciplinaWS;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -34,14 +35,16 @@ import retrofit2.Response;
 public class Home extends Fragment implements OnDisciplinaClickListener {
 
     //Cria o RecyclerView para os cards das disciplinas
-    RecyclerView rvDisciplina;
+    private RecyclerView rvDisciplina;
 
     //Armazena as disciplinas
-    List<Disciplina> disciplinas = new ArrayList<>();
+    private List<Disciplina> disciplinas = new ArrayList<>();
 
-    NavController navController;
+    private NavController navController;
 
-    String nomeDisciplina;
+    private String nomeDisciplina;
+
+    private AdapterDisciplinas adapterDisciplinas;
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -89,16 +92,64 @@ public class Home extends Fragment implements OnDisciplinaClickListener {
         //Usa uma classe chamada LinearLayoutManager pra organizar os cards de disciplina
         rvDisciplina.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
 
+        inserirIma();
 
         preencherDisciplinas();
 
-//        navController.navigate(R.id.action_home_to_conteudo);
         return view;
+    }
+
+    public static int dpToPx(Context context, int dp) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        int px = Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+        return px;
+    }
+
+    public void inserirIma(){
+        final SnapHelper snapHelper = new LinearSnapHelper();
+        snapHelper.attachToRecyclerView(rvDisciplina);
+
+        final int larguraOriginal = dpToPx(getActivity(), 200);
+        final int alturaOriginal = dpToPx(getActivity(), 297);
+        final int larguraFinal = dpToPx(getActivity(), 240);
+        final int alturaFinal = dpToPx(getActivity(), 357);
+
+        rvDisciplina.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+
+                if (layoutManager != null) {
+                    int recyclerViewCenter = recyclerView.getWidth() / 2;
+
+                    for (int i = 0; i < recyclerView.getChildCount(); i++) {
+                        View itemView = recyclerView.getChildAt(i);
+                        ViewGroup.LayoutParams layoutParams = itemView.getLayoutParams();
+
+                        int itemCenter = (itemView.getLeft() + itemView.getRight()) / 2;
+                        int distanceToCenter = Math.abs(itemCenter - recyclerViewCenter);
+
+                        // Calculate the fraction based on the distance to the center
+                        float fraction = 1.0f - (float) distanceToCenter / recyclerView.getWidth();
+
+                        // Interpolate between the original size and final size based on the fraction
+                        int newWidth = (int) (larguraOriginal + fraction * (larguraFinal - larguraOriginal));
+                        int newHeight = (int) (alturaOriginal + fraction * (alturaFinal - alturaOriginal));
+
+                        layoutParams.width = newWidth;
+                        layoutParams.height = newHeight;
+                        itemView.setLayoutParams(layoutParams);
+                    }
+                }
+            }
+        });
     }
 
     //Método Pra listar os cards de Disciplinas
     private void listarDisciplinas(){
-        AdapterDisciplinas adapterDisciplinas = new AdapterDisciplinas(getActivity(), disciplinas, this, navController);
+        adapterDisciplinas = new AdapterDisciplinas(getActivity(), disciplinas, this, navController);
         rvDisciplina.setAdapter(adapterDisciplinas);
     }
 
