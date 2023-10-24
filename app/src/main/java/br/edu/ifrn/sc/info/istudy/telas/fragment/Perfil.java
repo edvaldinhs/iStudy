251528f02074e8c08862dd3b9ce901a779f490e9
[edvaldinhs] <[edvaldohenriqueceu@gmail.com]>
2023-10-23 21:33:29 -0300

    #10 Layout, Troca de Foto(sem database) e Barra de Progresso(Pontos) Funcional.

--- app/src/main/java/br/edu/ifrn/sc/info/istudy/telas/fragment/Perfil.java
+++ app/src/main/java/br/edu/ifrn/sc/info/istudy/telas/fragment/Perfil.java
@@ -1,44 +1,48 @@
 package br.edu.ifrn.sc.info.istudy.telas.fragment;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import br.edu.ifrn.sc.info.istudy.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Perfil#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Perfil extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private int progressoAtual = 0;
+    private ProgressBar progresso;
+
+    private TextView tvPontos;
+
+    private CardView editarFoto;
+    private ImageView fotoAluno;
+
+    private ActivityResultLauncher<Intent> resultadoDaProcura;
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     public Perfil() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Perfil.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Perfil newInstance(String param1, String param2) {
         Perfil fragment = new Perfil();
         Bundle args = new Bundle();
@@ -63,9 +67,46 @@ public class Perfil extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_perfil, container, false);
 
+        preencherLauncherDeResultado();
+
         //Deixa o botão voltar visível
         getActivity().findViewById(R.id.voltar).setVisibility(View.VISIBLE);
 
+        tvPontos = view.findViewById(R.id.tvPontuacao);
+        progresso = view.findViewById(R.id.pgBarraPontuacao);
+
+        progressoAtual = 50;
+
+        tvPontos.setText(progressoAtual+"/100 pontos");
+        progresso.setProgress(progressoAtual);
+        progresso.setMax(100);
+
+        editarFoto = view.findViewById(R.id.cvEditarFoto);
+        fotoAluno = view.findViewById(R.id.ivFotoAluno);
+
+        editarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent procurarFoto = new Intent(Intent.ACTION_PICK);
+                procurarFoto.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                resultadoDaProcura.launch(procurarFoto);
+
+            }
+        });
+
         return view;
+    }
+    public void preencherLauncherDeResultado(){
+        resultadoDaProcura = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            fotoAluno.setImageURI(data.getData());
+                        }
+                    }
+                });
     }
 }
